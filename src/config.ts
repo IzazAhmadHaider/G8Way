0b01793ec1ffc683291aeb66a18a6b96beb6397d
izaz <izazahmadhaider0@gmail.com>
2025-01-09 16:47:19 +0500

    changes in config file

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@
 // Export reconstructed keys
 const apiKey: string = 'mik_sQZIWj36cB0rAWvHH51e20717';
 const apiSecret: string = 'mis_3O18TNSPfo1n5YR5ariICqwHqChPAoAgMnq3yV2KtMj7a270ff4';
-const mapId: string = '6748437a01c8d6000bfa9935'; // Optionally obfuscate if necessary
+const mapId: string = '6748437a01c8d6000bfa9935'; 
 
 
 export const mapConfig = {
