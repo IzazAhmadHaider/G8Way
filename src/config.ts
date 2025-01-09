e4950e2d46356c5e5e300c0906bc9a0269e81290
izaz <izazahmadhaider0@gmail.com>
2025-01-09 16:39:58 +0500

    Updated config

--- src/config.ts
+++ src/config.ts
@@ -1,17 +1,8 @@
-// Define obfuscated key parts
-const obfuscatedKeyParts: string[] = ['mik_sQZI', 'Wj36cB0r', 'AWvHH51e2', '0717'];
-const obfuscatedSecretParts: string[] = ['mis_3O18TN', 'SPfo1n5YR', '5ariICqwHq', 'ChPAoAgMnq3y'];
 
-/**
- * Reconstructs a key from an array of parts.
- * @param parts - Array of string parts to join.
- * @returns The reconstructed string key.
- */
-const reconstructKey = (parts: string[]): string => parts.join('');
 
 // Export reconstructed keys
-const apiKey: string = reconstructKey(obfuscatedKeyParts);
-const apiSecret: string = reconstructKey(obfuscatedSecretParts);
+const apiKey: string = 'mik_sQZIWj36cB0rAWvHH51e20717';
+const apiSecret: string = 'mis_3O18TNSPfo1n5YR5ariICqwHqChPAoAgMnq3yV2KtMj7a270ff4';
 const mapId: string = '6748437a01c8d6000bfa9935'; // Optionally obfuscate if necessary
 
 
